6c77b619af2f721369796ec4d930b8622f2d353a
Tanner Lemon <tannerlemon@Tanners-MacBook-Pro-2.local>
2022-01-04 16:42:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import jobs from "./Jobs.js";
+import searchIcon from "./imgs/search.svg";
+import forward from "./imgs/forward.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <div>JoBurque</div>
+        </div>
+        <div className="all-jobs">All jobs</div>
+      </div>
+      <div className="page-wrapper">
+        <div className="banner">
+          <h1>Tech Jobs in Albuquerque</h1>
+        </div>
+        <div className="search-box">
+          <img src={searchIcon} alt="search" />
+          <input id="search" placeholder="Search jobs..." />
+        </div>
+        <div className="jobs-and-department-wrapper">
+          <div className="departments-wrapper"></div>
+
+          <div className="job-wrapper">
+            {jobs.map((job) => {
+              return (
+                <div className="job">
+                  <div className="job-text">
+                    <h2 className="title">{job.title}</h2>
+                    <div className="company">{job.company}</div>
+                    <div className="overview">{job.overview}</div>
+                  </div>
+                  <img className="forward" src={forward} alt="forward" />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Jobs.js
@@ -0,0 +1,220 @@
+/*
+class Job {
+  constructor(
+    title,
+    city,
+    state,
+    company,
+    overview,
+    department,
+    fullDescription
+  ) {
+    this.title = title;
+    this.city = city;
+    this.state = state;
+    this.company = company;
+    this.overview = overview;
+    this.department = department;
+    this.fullDescription = fullDescription;
+  }
+}
+*/
+
+let jobs = [
+  {
+    title: "Robotics Technician/ Field Service",
+    city: "Albuquerque",
+    state: "NM",
+    company: "Build With Robots",
+    overview: "Develop and deploy robotic automation.",
+    department: "Engineering",
+    fullDescription: `Join a leading-edge robotics company – develop and deploy robotic automation. 
+
+        Job Responsibilities: 
+
+        Automation & robotic system deployment, testing, troubleshooting & repair 
+        Deployment and field service of automation solutions at customer sites 
+        Technical support, training and documentation 
+        Caring and responsive towards customers and coworkers 
+        Use knowledge of materials, parts, fabrication & assembly methods, quality & safety standards, to identify & implement changes to improve product designs, streamline manufacturing processes and reduce costs
+        Oversee safety assessments and design modifications for product safety and regulatory compliance
+        Troubleshoot new or existing product problems involving designs, materials, or processes
+        Qualifications: 
+
+        Minimum requirements: 
+
+        2+ years experience in technical field service and/or support 
+        Experience with pre-sales & post-sales technical support 
+        Good interpersonal skills for customer-facing role 
+        Strong technical background and troubleshooting skills 
+        Experience with automation, robotics and sensors 
+        Experienced with hands-on system integration and assembly work 
+        Ability to travel, perform physical activity, and work non-standard hours 
+        Preference for: 
+
+        Kindness and desire focus on the workflow of front-line workers (janitorial staff, factory workers, retail staff) 
+        Passionate about user experience and customer satisfaction 
+        Humor and patience  
+        Bilingual Spanish/English
+        Active or previous DOE or DOD clearance  
+    `,
+  },
+  {
+    title: "Mechanical Designer",
+    city: "Albuquerque",
+    state: "NM",
+    company: "Build With Robots",
+    overview:
+      "Join a leading-edge robotics company – marketing management for robotic solution sales & deployment.",
+    department: "Design",
+    fullDescription: `Join a leading-edge robotics company – support the development of robotic automation. 
+
+        Description & Responsibilities 
+
+        Job Responsibilities: 
+
+        Develop detailed design drawings and specifications for automation & robotic system using computer-assisted drafting (CAD) software 
+        Develop and analyze three-dimensional models using computer-aided design (CAD) software 
+        Develop fabrication & assembly procedures using CNC machine tools, 3D printers, etc. 
+        Review and analyze specifications, sketches, drawings, ideas, and related data to assess factors affecting component designs and manufacturing procedures  
+        Qualifications: 
+
+        Minimum requirements: 
+
+        5+ years drafting experience in computer-aided drafting and modeling 
+        Strong experience with Mechanical 3D CAD software such as SolidWorks or Fusion360 
+        Knowledge of design techniques and principles involved in production of precision technical plans, drawings, and models 
+        Knowledge of the practical application of engineering science and technology 
+        Preference for: 
+
+        Experience with 3D printers, CNC machine tools, etc. 
+        Experience with automation, robotics and sensors 
+    `,
+  },
+  {
+    title: "Robotics Software Engineer",
+    city: "Albuquerque",
+    state: "NM",
+    company: "Build With Robots",
+    overview: "Develop and deploy robotic automation.",
+    department: "Engineering",
+    fullDescription: `Build With Robots is looking for a passionate team player to grow our leading-edge robotics company. You’ll join a world-class team of software and hardware developers inventing and deploying collaborative robots that help people do their jobs in a variety of industries and applications.
+
+        Job Responsibilities:
+
+        Robot behavior programming using object-oriented and scripting programming languages (C/C++, Bash)
+        Development of software system architecture and procedures for implementation, testing, deployment and validation
+        Embedded system software development
+        Contribute to the design and implementation of our secure cloud-based data infrastructure
+        Develop graphical user interfaces, for internal and external customers
+        Remote monitoring, automation and control of robot deployments
+        Appropriate cybersecurity for all software systems
+        Process management & continuous improvement including:
+        Understanding and supporting project and team goals
+        Refinement of software development tools, systems and processes
+        Modular implementation for re-use and development efficiency
+        Documentation & process automation
+        Qualifications:
+
+        Minimum requirements:
+        Strong command of C and C++
+        Understanding of software system architecture
+        Experience with embedded software/firmware
+        Understanding of Object-Oriented Design
+        Experience with Linux development environment and knowledge of Linux system internals
+        4-year technical degree or equivalent experience/education
+        Preference for:
+        Experience with automation, robotics and/or sensors, ability to read schematics
+        Experience with Robot Operating System (ROS) middleware
+        Experience with Javascript and/or other web programming languages
+        Experience with cloud-based and/or distributed data processing systems
+        Understanding of IoT, embedded and/or cloud security concepts
+    `,
+  },
+  {
+    title: "Network & IT Administrator",
+    city: "Albuquerque",
+    state: "NM",
+    company: "Build With Robots",
+    overview:
+      "Manage information technology systems to support the deployment of robotic automation.",
+    department: "IT",
+    fullDescription: `Join a leading-edge robotics company – manage information technology systems to support the deployment of robotic automation. 
+
+        Job Responsibilities: 
+
+        Select, provision, maintain and administer computer equipment and networks including computer hardware, systems software, and application software
+        Diagnose, troubleshoot, and resolve hardware, software, or other network and system problems, and replace defective components when necessary
+        Define and coordinate security measures for information systems and create electronic data backup systems
+        Help to select, monitor, and maintain enterprise software applications (CRM, MRP, ERP)
+
+        Qualifications: 
+
+        5+ years experience in enterprise-level IT network administration
+        Experience with information security techniques, systems and procedures
+        Experience with CRM, MRP & ERP software applications
+        4-year technical degree or equivalent experience/education
+        Experience with Linux & Windows operating systems
+        Preference for experience with cloud-based data systems
+        Preference for IoT security knowledge and experience
+    `,
+  },
+  {
+    title: "Robotics Engineering Intern",
+    city: "Albuquerque",
+    state: "NM",
+    company: "Build With Robots",
+    overview: "Support the development of robotic automation.",
+    department: "Engineering",
+    fullDescription: `Internship Scope:
+        Spring 2022 semester 
+        At least 20 hours per week  
+        Work within a leading-edge robotics company – develop and deploy robotic automation 
+        Job Responsibilities & Tasks: 
+
+        Initial training as needed (e.g. UR Academy, Husky ROS tutorial)  
+        Automation Programming & Software Development (e.g. ROS/Linux & C/C++) 
+        System Integration (e.g. hardware integration, testing & validation) 
+        Engineering (e.g. Solution development & integration, testing & validation, light assembly) 
+        Qualifications: 
+
+        Demonstrated experience with software programming required 
+        Demonstrated technical problem-solving skills required 
+        Pursuing or completed a technical degree required 
+        Experience with embedded software & Linux desired 
+        Experience with high-technology and robotics desired 
+        Experience with CAD, machine tools, and 3D printers is a bonus 
+    `,
+  },
+  {
+    title: "Test Engineer",
+    city: "Albuquerque",
+    state: "NM",
+    company: "Build With Robots",
+    overview:
+      "Help build an innovative cloud-based platform for dashboarding, data analytics, and managing robots",
+    department: "Engineering",
+    fullDescription: `Join a leading-edge robotics company – develop & refine Test definition and implementation for the next generation of robotic automation.  
+
+        Job Responsibilities: 
+
+        Provide technical oversight for test & validation of automation & robotic solutions 
+        Create and implement test plans to prove in design decisions 
+        Support manufacturing engineering by helping define appropriate manufacturing acceptance tests. 
+        Support hardware and software engineering by helping define and implement appropriate validation tests required for the release of products and product updates. 
+        Qualifications: 
+
+        Minimum requirements: 
+        4-year technical degree or equivalent experience/education 
+        Demonstrated experience defining and implementing test plans. 
+        Ability to create and maintain technical reports 
+        Preference for: 
+
+        5+ years of test engineering experience 
+        Experience with automation, robotics and sensors 
+        Experienced with software programming   
+    `,
+  },
+];
+
+export default jobs;
